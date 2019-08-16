1bf8f8a116415729dff5eb564d27e6dd316602fa
jimf <jim.fang@ir.com>
2019-08-16 23:11:14 +1000

    exercises wk1

--- cis194/week1/jimfang/Homework01.hs
+++ cis194/week1/jimfang/Homework01.hs
@@ -1,6 +1,9 @@
 module Homework01 (
     toDigits,
-    toDigitsRev
+    toDigitsRev,
+    doubleEveryOther,
+    sumDigits,
+    validate
 ) where
 
 -- toDigits
@@ -13,3 +16,19 @@ toDigits num
 toDigitsRev :: Integer -> [Integer]
 toDigitRev = reverse . toDigits
 
+-- doubleEveryOther
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther []         = []
+doubleEveryOther [x]        = [x]
+doubleEveryOther [x:y:rest] = [x: y*2] ++ doubleEveryOther rest
+
+-- sumDigits
+sumDigits :: [Integer] -> Integer
+sumDigits = sum . concatMap . toDigits
+
+-- validate
+tenTimes :: Integer -> Bool
+tenTimes num = num `mod` 10 == 0
+
+validate :: Integer -> Bool
+validate = tenTimes . sumDigits . doubleEveryOther . toDigits 
